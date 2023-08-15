48050529b48fd57245879ad6261b9d5586b06613
Jisan Khan Sobuj <coderboysobuj@gmail.com>
2023-08-16 00:22:54 +0600

    Initial setup

---
+++ app/routes/_index.tsx
@@ -0,0 +1,36 @@
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import stylesUrl from '~/styles/index.css';
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Remix Jokes" },
+    { name: "description", content: "Jokes application by remix!" },
+  ];
+};
+
+export const links: LinksFunction = () => [
+  {
+    rel: 'stylesheet',
+    href: stylesUrl
+  }
+]
+
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
